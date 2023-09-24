5f965b3c3057886e9fe7bd23215586a880870330
T. Amburgey <timothy.amburgey@cognitronix-llc.com>
2023-09-24 15:31:11 -0400

    verify working SQL engine

--- src/server.py
+++ src/server.py
@@ -1,7 +1,14 @@
 from database_classes import HomeItem
 from flask import Flask, render_template, redirect, url_for, request
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 app = Flask(__name__)
+
+engin = create_engine('sqlite:///home_inventory.db', echo=True)
+Session = sessionmaker(bind=engin)
+session = Session()
+
 
 
 if __name__ == '__main__':
